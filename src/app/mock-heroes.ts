c19ba373ed73450816a2cb9ce0b004b425e5bbc7
hongbin <hongbin@htmimi.com>
2018-01-03 14:16:17 +0800

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: '超人' },
+  { id: 12, name: '好好先生' },
+  { id: 13, name: '海盗' },
+  { id: 14, name: 'helloWorld' },
+  { id: 15, name: 'vue' },
+  { id: 16, name: 'ReactNative' },
+  { id: 17, name: '三星' },
+  { id: 18, name: '联想' },
+  { id: 19, name: '午休枕' },
+  { id: 20, name: '钢骨' }
+];
